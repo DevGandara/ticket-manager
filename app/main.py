5ad5e09af7c44276d94fafffd72079283df5082a
DevGandara <david.gandara19@gmail.com>
2026-08-10 18:01:30 -0600

    feat: create ticket websocket

--- app/main.py
+++ app/main.py
@@ -1,22 +1,12 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from app.routers import tickets
+from app.websockets import tickets_ws
 
 app = FastAPI()
 
+app.include_router(tickets.router)
+app.include_router(tickets_ws.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept() # Aceptamos la conexión de WebSocket
-    try:
-        while True:
-            data = await websocket.receive_text() # Recibimos los datos del cliente
-    except WebSocketDisconnect:
-        print("Client disconnected")
---
+++ app/websockets/tickets_ws.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+router = APIRouter()
+
+@router.websocket("/ws/tickets")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
