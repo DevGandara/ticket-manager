cbdcbfbcf1a98af84323ff10652f17867c7d93cf
DevGandara <david.gandara19@gmail.com>
2026-08-13 12:39:56 -0600

    feat: update connection manager to support multiple tickets

--- app/services/connection_manager.py
+++ app/services/connection_manager.py
@@ -2,15 +2,21 @@ from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.connections = []
+        self.connections = {}
 
-    async def add_connection(self, websocket: WebSocket):
+    async def add_connection(self, ticket_id: str, websocket: WebSocket):
         await websocket.accept()
-        self.connections.append(websocket)
+        if ticket_id not in self.connections:
+            self.connections[ticket_id] = []
+        self.connections[ticket_id].append(websocket) # Cada websocket representa la conexión WebSocket de ese usuario como tal
 
-    async def remove_connection(self, websocket: WebSocket):
-        await websocket.close()
-        self.connections.remove(websocket)
+    async def remove_connection(self, ticket_id: str, websocket: WebSocket):
+        for ticket_id, connection in self.connections.items():
+            if websocket in connection:
+                connection.remove(websocket)
+                if not connection:  # Si no hay más conexiones para este ticket_id, eliminamos la entrada
+                    del self.connections[ticket_id]
+                break
 
     async def broadcast(self, message: str):
         for connection in self.connections:
